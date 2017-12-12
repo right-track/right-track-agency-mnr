31da799c2a9316352321348e4bf29d3dd3bc5c21
David Waring <dev@davidwaring.net>
2017-12-12 15:20:19 -0500

    Post-Compile: typo fix

--- db-build/src/postCompile.js
+++ db-build/src/postCompile.js
@@ -47,7 +47,7 @@ function postCompile(agencyOptions, db, callback) {
       // Set pickup_type and drop_off_type = 0 at all stops EXCEPT:
       // 125th St (4) and Fordham (56) White Plains (74) and Croton-Harmon (33)
       console.log("    ... Fixing pickup/drop-off types");
-      db.exec("UPDATE gtfs_stop_times SET pickgup_type=0, drop_off_type=0 WHERE stop_id <> 4 AND stop_id <> 56 AND stop_id <> 74 AND stop_id <> 33;");
+      db.exec("UPDATE gtfs_stop_times SET pickup_type=0, drop_off_type=0 WHERE stop_id <> 4 AND stop_id <> 56 AND stop_id <> 74 AND stop_id <> 33;");
 
       db.exec("COMMIT", function() {
         return callback();
