b41fcc13712dca5404245bb13cf205ce55b502fc
David Waring <dev@davidwaring.net>
2017-12-23 21:04:39 -0500

    Post Update: more robust appending
    Append SLE data to GTFS data by column header

--- db-build/src/postUpdate.js
+++ db-build/src/postUpdate.js
@@ -2,11 +2,23 @@
 
 const fs = require('fs');
 const path = require('path');
+const rl = require('readline');
 
 
 // LOCATION OF GTFS DATA
 const SLE_DIR = '../sle/';
 const MNR_DIR = '../gtfs/';
+
+// LIST OF SLE FILES TO COPY
+const FILES = [
+  'agency.txt', 
+  'calendar_dates.txt', 
+  'calendar.txt',
+  'routes.txt',
+  'stop_times.txt',
+  'stops.txt',
+  'trips.txt'
+];
 
 
 /**
@@ -20,9 +32,8 @@ function postUpdate(agencyOptions, callback) {
   if ( agencyOptions.updateComplete === true ) {
     console.log("    Copying SLE GTFS data into MNR data...");
 
-    // Get paths to directories
+    // Get Directory Paths
     let sleDir = path.normalize(__dirname + '/' + SLE_DIR);
-    let mnrDir = path.normalize(__dirname + '/' + MNR_DIR);
 
     // Make sure SLE Directory exists
     if ( fs.existsSync(sleDir) ) {
@@ -30,34 +41,128 @@ function postUpdate(agencyOptions, callback) {
       // Get files in directory
       let files = fs.readdirSync(sleDir);
 
-      // Parse each of the files
-      for ( let i = 0; i < files.length; i++ ) {
-        let filename = files[i];
-        let sleFile = path.normalize(sleDir + '/' + filename);
-        let mnrFile = path.normalize(mnrDir + '/' + filename);
-
-        // Make sure file is a txt file
-        if ( filename.indexOf('.txt') > -1 ) {
-          console.log("    ... " + filename);
-          
-          // Read SLE File
-          let data = fs.readFileSync(sleFile, 'utf-8');
-          let lines = data.split('\n');
-          lines = lines.slice(1);
-          lines = lines.join('\n');
-          
-          // Append to MNR File
-          fs.appendFileSync(mnrFile, lines, 'utf-8');
-
-        }
-      }
+      // Process the Files
+      _process(0, files, callback);
 
     }
     
   }
-  
-  // Return to the main script
-  return callback();
+}
+
+
+/**
+ * Start processing the SLE Files
+ */
+function _process(count, files, callback) {
+  if ( count < files.length ) {
+    _processFile(files[count], function() {
+      _process(count+1, files, callback);
+    });
+  }
+  else {
+    return callback();
+  }
+}
+
+
+/**
+ * Process a specific SLE File
+ */
+function _processFile(filename, callback) {
+
+  // Make sure file is a txt file
+  if ( FILES.indexOf(filename) > -1 ) {
+    console.log("    ... " + filename);
+
+    // Set file paths
+    let sleFile = path.normalize(__dirname + '/' + SLE_DIR + '/' + filename);
+    let mnrFile = path.normalize(__dirname + '/' + MNR_DIR + '/' + filename);
+    
+
+    // Read MNR file for header fields
+    let mnr = fs.readFileSync(mnrFile, 'utf-8');
+    let mnrHeaders = mnr.split(/\r|\n/)[0].split(',');
+    for ( let i = 0; i < mnrHeaders.length; i++ ) {
+      mnrHeaders[i] = mnrHeaders[i].trim();
+    }
+
+
+    // Set SLE Headers
+    let sleHeaders = [];
+    let readHeaders = false;
+
+    // Set SLE Data
+    let sleData = [];
+
+    // Set SLE RL Interface
+    let rd = rl.createInterface({
+      input: fs.createReadStream(sleFile),
+      crlfDelay: Infinity
+    });
+
+    // Read SLE File Line by Line
+    rd.on('line', function(line) {
+      if ( line.toString().length > 0 ) {
+        let data = line.split(',');
+        if ( !readHeaders ) {
+          readHeaders = true;
+          for ( let i = 0; i < data.length; i++ ) {
+            sleHeaders[i] = data[i].trim();
+          }
+        }
+        else {
+          let dataObj = {};
+          for ( let i = 0; i < data.length; i++ ) {
+            dataObj[sleHeaders[i]] = data[i];
+          }
+          sleData.push(dataObj);
+        }
+      }
+    });
+
+    // Parse the read data
+    rd.on('close', function() {
+
+      // Output data to append to MNR File
+      let output = [];
+
+      // Parse each of the sleData items
+      for ( let i = 0; i < sleData.length; i++ ) {
+        let sle = sleData[i];
+        let line = [];
+
+        // Loop through MNR Header
+        for ( let j = 0; j < mnrHeaders.length; j++ ) {
+          let header = mnrHeaders[j];
+          let item = sle[header];
+          if ( item === undefined ) {
+            item = '';
+          }
+          line.push(item);
+        }
+
+        // Collapse line array
+        line = line.join(',');
+
+        // Add line to output
+        output.push(line);
+
+      }
+
+      // Collapse output array
+      output = output.join('\n');
+
+      // Append output to MNR file
+      fs.appendFileSync(mnrFile, output, 'utf-8');
+
+      return callback();
+    });
+
+  }
+  else {
+    return callback();
+  }
+
 }
 
 
